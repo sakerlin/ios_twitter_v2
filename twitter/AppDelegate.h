22e096f44ad4577ffabdcdd43c75eef1fde9f8b1
saker <saker@yahoo-inc.com>
2015-06-25 11:02:10 +0800

    Initial Commit

---
+++ twitter/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  twitter
+//
+//  Created by Saker Lin on 2015/6/25.
+//  Copyright (c) 2015年 Saker Lin. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ twitter/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  twitter
+//
+//  Created by Saker Lin on 2015/6/25.
+//  Copyright (c) 2015年 Saker Lin. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
